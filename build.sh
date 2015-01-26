a6334bbf88b4a077bc5bf760c5f472a575c1c7fa
Tim Brown <tpbrown@gmail.com>
2015-01-26 15:37:22 -0800

    Just save the build info as a pseudo artifact

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+echo $GO_PIPELINE_NAME build $GO_PIPELINE_LABEL triggered
 echo COMPILE..
 echo UNIT TEST...
+echo ${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}-${GO_STAGE_COUNTER}>artifact.txt
 exit 0
