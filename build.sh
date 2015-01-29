bfa55ec3aea09e29e4f4ad4e5c171c82056e5708
Tim Brown <tpbrown@gmail.com>
2015-01-28 22:45:41 -0800

    Cleaning up artifacts.  use diretories so we dont care about versions in file names

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 echo '(pseudo build script.  Pass any parameter to fail.)'
-mkdir -p results
-rm -f results/*
-artifact_name=${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}.${GO_STAGE_COUNTER}.txt
+rm -f results
+mkdir -p results/deployables
+mkdir -p results/reports
+artifact_name="deployables/${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}.${GO_STAGE_COUNTER}.txt"
 echo $GO_PIPELINE_NAME build $GO_PIPELINE_LABEL triggered
 echo COMPILE..
 echo UNIT TEST...
-echo "${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}.${GO_STAGE_COUNTER}(${GO_TO_REVISION})">results/${artifact_name}
+echo "${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}.${GO_STAGE_COUNTER}(${GO_TO_REVISION})">${artifact_name}
 if [ -z "$1" ]
 then
-   cp -f fake-outputs/junit-pass.xml results/junit.xml
+   cp -f fake-outputs/junit-pass.xml results/reports/junit.xml
    exit 0
 fi
-cp -f fake-outputs/junit-fail.xml results/junit.xml
+cp -f fake-outputs/junit-fail.xml results/reports/junit.xml
 exit 1
