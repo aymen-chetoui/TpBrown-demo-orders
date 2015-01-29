fdd3182d01fb3694aded252c4e6f11efd80cc70d
Tim Brown <tpbrown@gmail.com>
2015-01-28 21:05:53 -0800

    Artifacts should also be in results

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 echo '(pseudo build script.  Pass any parameter to fail.)'
-rm -f artifact.txt
+mkdir -p results
+rm -f results/artifact.txt
+rm -f results/junit.xml
 echo $GO_PIPELINE_NAME build $GO_PIPELINE_LABEL triggered
 echo COMPILE..
 echo UNIT TEST...
-echo ${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}-${GO_STAGE_COUNTER}>artifact.txt
-mkdir -p results
+echo ${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}-${GO_STAGE_COUNTER}>results/artifact.txt
 if [ -z "$1" ]
 then
    cp -f fake-outputs/junit-pass.xml results/junit.xml
