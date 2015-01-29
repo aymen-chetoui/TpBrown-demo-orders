92124c68749f891f4255a949f53030fd876c0cff
Tim Brown <tpbrown@gmail.com>
2015-01-28 22:28:22 -0800

    Use pipeline information for artifact naming.

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 echo '(pseudo build script.  Pass any parameter to fail.)'
 mkdir -p results
-rm -f results/artifact.txt
-rm -f results/junit.xml
+rm -f results/*
+artifact_name=${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}.${GO_STAGE_COUNTER}.txt
 echo $GO_PIPELINE_NAME build $GO_PIPELINE_LABEL triggered
 echo COMPILE..
 echo UNIT TEST...
-echo ${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}-${GO_STAGE_COUNTER}>results/artifact.txt
+echo ${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}.${GO_STAGE_COUNTER}(${GO_TO_REVISION})>results/${artifact_name}
 if [ -z "$1" ]
 then
    cp -f fake-outputs/junit-pass.xml results/junit.xml
