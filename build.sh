3552af66ab34783f36e1d802f752942b962a5cc9
Tim Brown <tpbrown@gmail.com>
2015-01-28 20:59:44 -0800

    fake junit output on pass/fail

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ echo UNIT TEST...
 echo ${GO_PIPELINE_NAME}-${GO_PIPELINE_LABEL}-${GO_STAGE_COUNTER}>artifact.txt
 if [ -z "$1" ]
 then
+   cp -f fake-outputs/junit-pass.xml results/junit.xml
    exit 0
 fi
+cp -f fake-outputs/junit-fail.xml results/junit.xml
 exit 1
